22b941a8ba52d78ae3ae6206344879daf03d511e
Mattia Robuschi Caprara <mattia.r.c@icloud.com>
2021-11-15 23:49:07 +0100

    Program created

---
+++ CifCesareRobu/main.py
@@ -0,0 +1,48 @@
+"""Fai un menu con:
+1) critta
+2) decritta
+3) esci
+
+Crittare e decrittare con linguaggio cesare"""
+alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'I', 'M', 'N', 'O', 'P', 'O', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z']
+
+
+def main():
+    cont = 0
+
+    while cont != 3:
+        print('1 = cripta\n')
+        print('2 = decripta\n')
+        print('3 = esc\n')
+        cont = int(input(">> "))
+        critt()
+
+
+def critt():
+    daCif = str(input("Inserisci il messaggio da cifrare: "))
+    key = int(input("Inserisci la chiave di crittazione: "))
+    carattere = ''
+    testoCritt = ""
+    daCif = daCif.upper()
+
+    for charMess in range(len(daCif)):
+        index = alpha.index(daCif[charMess])
+        for cntK in range(key):
+            index = index + 1
+
+            if index is len(alpha):
+                index = 0
+        carattere = alpha[index]
+        testoCritt += carattere
+
+    print('Messaggio crittato: ' + testoCritt)
+
+
+def decritt():
+    daDec = str(input("Inserisci il messaggio da decifrare: "))
+    key = int(input("Inserisci la chiave di crittazione: "))
+
+
+main()
+
